8c3703c7bc95a144345464417e5b6fd48b558aec
udayveer923 <124018933+udayveer923@users.noreply.github.com>
2024-05-22 19:43:34 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Firstpage from "./components/Firstpage"
+import Secondpage from "./components/Secondpage"
+import Thirdpage from "./components/Thirdpage"
+import Fourthpage from "./components/Fourthpage"
+import Fifthpage from "./components/Fifthpage"
+import "./App.css"
+
+function App() {
+
+  return (
+    <div className="psdfreebies-com">
+    <Firstpage/>
+    <Secondpage/>
+    <Thirdpage/>
+    <Fourthpage/>
+    <Fifthpage/>
+    </div>
+  )
+}
+
+export default App
